df1c09324df083ffa425e21d86dad47d9a2b0421
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-11-29 01:28:40 +0530

    Login with Cookies and me + Logout routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,18 @@
-import { validate } from "class-validator";
+import { isEmpty, validate } from "class-validator";
 import { Request, Response, Router } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
 
 import { User } from "../entities/User";
+import auth from "../middleware/auth";
 
 const register = async (req: Request, res: Response) => {
   console.log(req.body);
   const { email, username, password } = req.body;
 
   try {
-    // TODO: Validate data
+    // Validate data
     let errors: Record<string, any> = {};
     const emailUser = await User.findOne({ email });
     const usernameUser = await User.findOne({ username });
@@ -18,7 +22,7 @@ const register = async (req: Request, res: Response) => {
 
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
-    // TODO: Create user
+    // Create user
     const user = new User({ email, username, password });
 
     const error = await validate(user);
@@ -26,7 +30,7 @@ const register = async (req: Request, res: Response) => {
 
     await user.save();
 
-    // TODO: Return user
+    // Return user
     return res.json(user);
   } catch (error) {
     console.log(error);
@@ -35,7 +39,68 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    let errors: any = {};
+
+    if (isEmpty(username)) errors.username = "Username must not be empty";
+    if (isEmpty(password)) errors.password = "Password must not be empty";
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+    if (!passwordMatches)
+      return res.status(401).json({ password: "Password is incorrect" });
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+    res.set(
+      "Set-Cookie",
+      cookie.serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 3600,
+        path: "/",
+      })
+    );
+
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+
+    return res.status(400).json(err);
+  }
+};
+
+const me = async (_: Request, res: Response) => {
+  return res.json(res.locals.user);
+};
+
+const logout = async (_: Request, res: Response) => {
+  res.set(
+    "Set-Cookie",
+    cookie.serialize("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      expires: new Date(0),
+      path: "/",
+    })
+  );
+
+  return res.json({ success: true });
+};
+
 const router = Router();
 router.post("/register", register);
+router.post("/login", login);
+router.get("/me", auth, me);
+router.get("/logout", auth, logout);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express, { json } from "express";
 import morgan from "morgan";
-import { resolve } from "path";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
 
 import authRoutes from "./routes/auth";
 import trim from "./middleware/trim";
@@ -11,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(trim);
+app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/auth", authRoutes);
