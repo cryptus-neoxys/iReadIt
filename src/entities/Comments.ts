98d5b8bdad5263a90bd3830552bdd1e2e8c65fed
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-04 21:06:02 +0530

    Comments

---
+++ src/entities/Comments.ts
@@ -0,0 +1,43 @@
+import {
+  BeforeInsert,
+  Column,
+  Entity as TOEntity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+} from "typeorm";
+import { makeId } from "../util/heplers";
+
+import Entity from "./Entity";
+import Post from "./Post";
+import User from "./User";
+
+@TOEntity("comments")
+export default class Comment extends Entity {
+  constructor(comment: Partial<Comment>) {
+    super();
+    Object.assign(this, comment);
+  }
+
+  @Index()
+  @Column()
+  identifier: string; // 7 Character Id
+
+  @Column({ nullable: false, type: "text" })
+  body: string;
+
+  @Column()
+  username: string;
+
+  @ManyToOne(() => User, { nullable: false })
+  @JoinColumn({ name: "username", referencedColumnName: "username" })
+  user: User;
+
+  @ManyToOne(() => Post, (post) => post.comments, { nullable: false })
+  post: Post;
+
+  @BeforeInsert()
+  makeIdAndSlug() {
+    this.identifier = makeId(8);
+  }
+}
