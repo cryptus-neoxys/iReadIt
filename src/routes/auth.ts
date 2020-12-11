1ee7f5db6cdaacb2fd60fcf004ce5cd89cbfb00f
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-12 04:18:09 +0530

    error++ , minor fix

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,6 +7,13 @@ import cookie from "cookie";
 import User from "../entities/User";
 import auth from "../middleware/auth";
 
+const mapErrors = (errors: any) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints)[0][1];
+    return prev;
+  }, {});
+};
+
 const register = async (req: Request, res: Response) => {
   console.log(req.body);
   const { email, username, password } = req.body;
@@ -20,13 +27,17 @@ const register = async (req: Request, res: Response) => {
     if (emailUser) errors.email = "Email is already taken";
     if (usernameUser) errors.username = "Username is already taken";
 
-    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
 
     // Create user
     const user = new User({ email, username, password });
 
-    const error = await validate(user);
-    if (error.length > 0) return res.status(400).json({ error });
+    errors = await validate(user);
+    if (errors.length > 0) {
+      return res.status(400).json(mapErrors(errors));
+    }
 
     await user.save();
 
@@ -51,7 +62,7 @@ const login = async (req: Request, res: Response) => {
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     const user = await User.findOne({ username });
-    if (!user) return res.status(404).json({ error: "User not found" });
+    if (!user) return res.status(404).json({ username: "User not found" });
 
     const passwordMatches = await bcrypt.compare(password, user.password);
     if (!passwordMatches)
