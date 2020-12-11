9a4114ecaf6670e510a8b7142947d74049ec1671
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-12 04:19:30 +0530

    cleared index

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,19 +1,11 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>iReadit: The front page of the internet</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
     </div>
   );
 }
