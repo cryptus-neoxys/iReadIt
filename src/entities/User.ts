12a8f3a57f6f231c814a761c0dc9889a3c9a3430
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-12 04:17:47 +0530

    errror modificatins

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -20,19 +20,19 @@ export default class User extends Entity {
   }
 
   @Index()
-  @Length(3, 255)
-  @IsEmail()
+  @IsEmail(undefined, { message: "Must be a valid email address" })
+  @Length(1, 255, { message: "Must not be empty" })
   @Column({ unique: true })
   email: string;
 
   @Index()
-  @Length(3, 255)
+  @Length(3, 255, { message: "Must be at least 3 characters long" })
   @Column({ unique: true })
   username: string;
 
   @Exclude()
   @Column()
-  @Length(6, 255)
+  @Length(6, 255, { message: "Must be at least 6 characters long" })
   password: string;
 
   @OneToMany(() => Post, (post) => post.user)
