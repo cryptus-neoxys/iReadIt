53fe18601a8af369705faa884b1e0983c23f0281
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-16 05:02:56 +0530

    added voting (misc) Route

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ dotenv.config();
 import authRoutes from "./routes/auth";
 import subRoutes from "./routes/subs";
 import postRoutes from "./routes/posts";
+import miscRoutes from "./routes/misc";
 import trim from "./middleware/trim";
 
 const app = express();
@@ -31,6 +32,7 @@ app.get("/", (_, res) => res.send("Hello World"));
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/subs", subRoutes);
+app.use("/api/misc", miscRoutes);
 
 app.listen(PORT, async () => {
   console.log(`Server running at: http://localhost:${PORT}`);
