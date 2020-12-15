259f5caabbb1b1851bd3b7f7563ca9d302b16dc2
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-16 05:03:59 +0530

    POst - VOte relation

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,4 +1,4 @@
-import { Expose } from "class-transformer";
+import { Exclude, Expose } from "class-transformer";
 import {
   Entity as TOEntity,
   Column,
@@ -11,10 +11,11 @@ import {
 } from "typeorm";
 
 import { makeId, slugify } from "../util/heplers";
-import Comment from "./Comments";
+import Comment from "./Comment";
 import Entity from "./Entity";
 import Sub from "./Sub";
 import User from "./User";
+import Vote from "./Vote";
 
 @TOEntity("posts")
 export default class Post extends Entity {
@@ -54,8 +55,26 @@ export default class Post extends Entity {
   @OneToMany(() => Comment, (comment) => comment.post)
   comments: Comment[];
 
+  @Exclude()
+  @OneToMany(() => Vote, (vote) => vote.post)
+  votes: Vote[];
+
   @Expose() get url(): string {
     return `/r/${this.subName}/${this.identifier}/${this.slug}`;
+  }
+
+  @Expose() get commentCount(): number {
+    return this.comments?.length;
+  }
+
+  @Expose() get voteScore(): number {
+    return this.votes.reduce((prev, curr) => prev + (curr.value || 0), 0);
+  }
+
+  protected userVote: number;
+  setUserVote(user: User) {
+    const index = this.votes?.findIndex((v) => v.username === user.username);
+    this.userVote = index > -1 ? this.votes[index].value : 0;
   }
 
   // protected url: string;
