2950a83324d04cd3875429adbb2c59097908d9f8
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-16 03:32:47 +0530

    Home page with posts

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,106 @@
+import Axios from "axios";
 import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { GetServerSideProps } from "next";
+
+import { Post } from "../types";
+
+dayjs.extend(relativeTime);
 
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    Axios.get("/posts")
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
+    <div className="pt-12">
       <Head>
         <title>iReadit: The front page of the internet</title>
       </Head>
+      <div className="container flex pt-4">
+        <div className="w-160">
+          {/* Post Feed */}
+          {posts.map((post) => (
+            <div key={post.identifier} className="flex mb-4 bg-white rounded">
+              {/* Vote Section */}
+              <div className="w-10 text-center bg-gray-100 rounded-l">
+                <p>V</p>
+              </div>
+              {/* Post Data Section */}
+              <div className="w-full p-2">
+                <div className="flex items-center">
+                  <Link href={`/r/${post.subName}`}>
+                    <>
+                      <img
+                        className="w-6 h-6 mr-1 cursor-pointer"
+                        src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y"
+                        alt="avatar"
+                      />
+                      <a className="text-xs font-bold cursor-pointer hover:underline">
+                        {post.subName}
+                      </a>
+                    </>
+                  </Link>
+                  <p className="text-xs text-gray-500">
+                    <span className="mx-1">•</span>
+                    Posted by
+                    <Link href={`/u/${post.username}`}>
+                      <a className="mx-1 hover:underline">/u/user</a>
+                    </Link>
+                    <Link href={post.url}>
+                      <a className="mx-1 hover:underline">
+                        {dayjs(post.createdAt).fromNow()}
+                      </a>
+                    </Link>
+                  </p>
+                </div>
+                <Link href={post.url}>
+                  <a className="my-1 text-lg font-medium">{post.title}</a>
+                </Link>
+                {post.body && <p className="my-1 text-sm">{post.body}</p>}
+
+                <div className="flex">
+                  <Link href={post.url}>
+                    <a>
+                      <div className="px-1 mr-2 text-gray-500 rounded cursor-pointer hover:bg-gray-200">
+                        <i className="mr-1 fas fa-comment-alt fa-xs"></i>
+                        <span className="font-bold">20 Comments</span>
+                      </div>
+                    </a>
+                  </Link>
+                  <div className="px-1 mr-2 text-gray-500 rounded cursor-pointer hover:bg-gray-200">
+                    <i className="mr-1 fas fa-bookmark fa-xs"></i>
+                    <span className="font-bold">Bookmark</span>
+                  </div>
+                  <div className="px-1 mr-2 text-gray-500 rounded cursor-pointer hover:bg-gray-200">
+                    <i className="mr-1 fas fa-share fa-xs"></i>
+                    <span className="font-bold">Share</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* SideBar */}
+        <div>SideBar</div>
+      </div>
     </div>
   );
 }
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   try {
+//     const res = await Axios.get("/posts");
+
+//     return { props: { posts: res.data } };
+//   } catch (err) {
+//     return { props: { error: "Something went wrong" } };
+//   }
+// };
