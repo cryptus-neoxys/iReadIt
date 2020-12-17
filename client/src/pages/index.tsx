2c0b5467fcdb5c5b625575edc8ef70513a63f610
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-18 02:29:20 +0530

    index uses PostCard cmp

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,6 +7,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { GetServerSideProps } from "next";
 
 import { Post } from "../types";
+import PostCard from "../components/PostCard";
 
 dayjs.extend(relativeTime);
 
@@ -28,64 +29,7 @@ export default function Home() {
         <div className="w-160">
           {/* Post Feed */}
           {posts.map((post) => (
-            <div key={post.identifier} className="flex mb-4 bg-white rounded">
-              {/* Vote Section */}
-              <div className="w-10 text-center bg-gray-100 rounded-l">
-                <p>V</p>
-              </div>
-              {/* Post Data Section */}
-              <div className="w-full p-2">
-                <div className="flex items-center">
-                  <Link href={`/r/${post.subName}`}>
-                    <img
-                      className="w-6 h-6 mr-1 cursor-pointer"
-                      src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y"
-                      alt="avatar"
-                    />
-                  </Link>
-                  <Link href={`/r/${post.subName}`}>
-                    <a className="text-xs font-bold cursor-pointer hover:underline">
-                      {post.subName}
-                    </a>
-                  </Link>
-                  <p className="text-xs text-gray-500">
-                    <span className="mx-1">•</span>
-                    Posted by
-                    <Link href={`/u/${post.username}`}>
-                      <a className="mx-1 hover:underline">/u/user</a>
-                    </Link>
-                    <Link href={post.url}>
-                      <a className="mx-1 hover:underline">
-                        {dayjs(post.createdAt).fromNow()}
-                      </a>
-                    </Link>
-                  </p>
-                </div>
-                <Link href={post.url}>
-                  <a className="my-1 text-lg font-medium">{post.title}</a>
-                </Link>
-                {post.body && <p className="my-1 text-sm">{post.body}</p>}
-
-                <div className="flex">
-                  <Link href={post.url}>
-                    <a>
-                      <div className="px-1 mr-2 text-gray-500 rounded cursor-pointer hover:bg-gray-200">
-                        <i className="mr-1 fas fa-comment-alt fa-xs"></i>
-                        <span className="font-bold">20 Comments</span>
-                      </div>
-                    </a>
-                  </Link>
-                  <div className="px-1 mr-2 text-gray-500 rounded cursor-pointer hover:bg-gray-200">
-                    <i className="mr-1 fas fa-bookmark fa-xs"></i>
-                    <span className="font-bold">Bookmark</span>
-                  </div>
-                  <div className="px-1 mr-2 text-gray-500 rounded cursor-pointer hover:bg-gray-200">
-                    <i className="mr-1 fas fa-share fa-xs"></i>
-                    <span className="font-bold">Share</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PostCard post={post} key={post.identifier} />
           ))}
         </div>
         {/* SideBar */}
