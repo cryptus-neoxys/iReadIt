8cc6ca9b0079197230181ff3b2eb4b9364ce3a55
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-20 19:21:20 +0530

    login authRouted with auth ctx

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-import { useAuthDispatch } from "../context/auth";
+import { useAuthDispatch, useAuthState } from "../context/auth";
 
 import InputGroup from "../components/InputGroup";
 
@@ -14,8 +14,10 @@ export default function Home() {
   const [errors, setErrors] = useState<any>({});
 
   const dispatch = useAuthDispatch();
+  const { authenticated } = useAuthState();
 
   const router = useRouter();
+  if (authenticated) router.push("/");
 
   const submitForm = async (event: FormEvent) => {
     event.preventDefault();
