de9cab6db151cc17f5e13d1235d1b9931ba24058
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-22 02:41:37 +0530

    minor fix 500 -> 404 since findOrFail

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,6 +1,7 @@
 import { isEmpty } from "class-validator";
 import { Request, Response, Router } from "express";
 import { getRepository } from "typeorm";
+import Post from "../entities/Post";
 import Sub from "../entities/Sub";
 
 import User from "../entities/User";
@@ -41,8 +42,33 @@ const createSub = async (req: Request, res: Response) => {
   }
 };
 
+const getSub = async (req: Request, res: Response) => {
+  const name = req.params.name;
+
+  try {
+    const sub = await Sub.findOneOrFail({ name });
+    const posts = await Post.find({
+      where: { sub },
+      relations: ["comments", "votes"],
+      order: { createdAt: "DESC" },
+    });
+
+    sub.posts = posts;
+
+    if (res.locals.user) {
+      sub.posts.forEach((p) => p.setUserVote(res.locals.user));
+    }
+
+    return res.json(sub);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ sub: "Sub not found" });
+  }
+};
+
 const router = Router();
 
 router.post("/", user, auth, createSub);
+router.get("/:name", user, getSub);
 
 export default router;
