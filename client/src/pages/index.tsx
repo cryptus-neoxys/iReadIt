b48afd0dead5a1777ac980ea6ef9802622897fff
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-22 02:06:46 +0530

    posts fetched with useSWR

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,7 @@
-import Axios from "axios";
 import Head from "next/head";
-import Link from "next/link";
-import { Fragment, useEffect, useState } from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { GetServerSideProps } from "next";
+import useSWR from "swr";
 
 import { Post } from "../types";
 import PostCard from "../components/PostCard";
@@ -12,13 +9,7 @@ import PostCard from "../components/PostCard";
 dayjs.extend(relativeTime);
 
 export default function Home() {
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    Axios.get("/posts")
-      .then((res) => setPosts(res.data))
-      .catch((err) => console.log(err));
-  }, []);
+  const { data: posts } = useSWR("/posts");
 
   return (
     <div className="pt-12">
@@ -28,7 +19,7 @@ export default function Home() {
       <div className="container flex pt-4">
         <div className="w-160">
           {/* Post Feed */}
-          {posts.map((post) => (
+          {posts?.map((post: Post) => (
             <PostCard post={post} key={post.identifier} />
           ))}
         </div>
