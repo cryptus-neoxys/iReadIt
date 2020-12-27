4e1b7aea52b1a56de32aebb3c66500976d3cde5d
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-27 21:06:41 +0530

    sub interface

--- client/src/types.ts
+++ client/src/types.ts
@@ -20,3 +20,18 @@ export interface User {
   createdeAt: string;
   updatedAt: string;
 }
+
+export interface Sub {
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  title: string;
+  description: string;
+  imageUrn: string;
+  bannerUrn: string;
+  username: string;
+  posts: Post[];
+  // virtual fields
+  imageUrl: string;
+  bannerUrl: string;
+}
