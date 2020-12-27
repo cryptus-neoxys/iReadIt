f80e99ac3148482d40fbbe13c7b7c71020655e34
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-27 20:56:39 +0530

    virtual fields for image and banner URL

--- src/entities/Sub.ts
+++ src/entities/Sub.ts
@@ -1,3 +1,4 @@
+import { Expose } from "class-transformer";
 import {
   Entity as TOEntity,
   Column,
@@ -43,4 +44,18 @@ export default class Sub extends Entity {
 
   @OneToMany(() => Post, (post) => post.sub)
   posts: Post[];
+
+  @Expose()
+  get imageUrl(): string {
+    return this.imageUrn
+      ? `${process.env.APP_URL}/images/${this.imageUrn}`
+      : "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y";
+  }
+
+  @Expose()
+  get bannerUrl(): string | undefined {
+    return this.bannerUrn
+      ? `${process.env.APP_URL}/images/${this.bannerUrn}`
+      : undefined;
+  }
 }
