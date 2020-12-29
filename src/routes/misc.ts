08298376b800e219d00f8eb66aec9bcc3d65f1ad
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-30 03:24:26 +0530

    topsubs route added

--- src/routes/misc.ts
+++ src/routes/misc.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from "express";
+import { getConnection } from "typeorm";
 import Comment from "../entities/Comment";
 import Post from "../entities/Post";
+import Sub from "../entities/Sub";
 import User from "../entities/User";
 import Vote from "../entities/Vote";
 
@@ -61,7 +63,29 @@ const vote = async (req: Request, res: Response) => {
   }
 };
 
+const topSubs = async (_: Request, res: Response) => {
+  try {
+    const imgUrlExp = `COALESCE('${process.env.APP_URL}/images/' || s."imageUrn", 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y')`;
+    const subs = await getConnection()
+      .createQueryBuilder()
+      .select(
+        `s.title, s.name, ${imgUrlExp} as "imageUrl", count(p.id) as "postCount"`
+      )
+      .from(Sub, "s")
+      .leftJoin(Post, "p", `s.name = p."subName"`)
+      .groupBy('s.title, s.name, "imageUrl"')
+      .orderBy(`"postCount"`, "DESC")
+      .limit(5)
+      .execute();
+
+    return res.json(subs);
+  } catch (err) {
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
 const router = Router();
 router.post("/vote", user, auth, vote);
+router.get("/top-subs", topSubs);
 
 export default router;
