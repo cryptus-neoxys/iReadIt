55a37af0718b63582cd16e8c52c348ac0e0ce656
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-30 03:58:55 +0530

    push.back() on login instead of redirect to home

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
       dispatch("LOGIN", res.data);
 
-      router.push("/");
+      router.back();
     } catch (err) {
       setErrors(err.response.data);
     }
