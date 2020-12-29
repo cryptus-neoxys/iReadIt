0dba69a001c73da44b99eca8801f010a0c474d81
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-30 03:40:02 +0530

    index Side Bar done

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { Post, Sub } from "../types";
 import PostCard from "../components/PostCard";
 import { Fragment } from "react";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -39,14 +40,22 @@ export default function Home() {
                 <div
                   key={sub.name}
                   className="flex items-center px-4 py-2 text-xs border-b-4">
-                  <div className="rounded-full">
-                    <Image
-                      src={sub.imageUrl}
-                      alt="Sub"
-                      width={24}
-                      height={24}
-                    />
+                  <div className="mr-2 overflow-hidden rounded-full cursor-pointer">
+                    <Link href={`/r/${sub.name}`}>
+                      <Image
+                        src={sub.imageUrl}
+                        alt="Sub"
+                        width={24}
+                        height={24}
+                      />
+                    </Link>
                   </div>
+                  <Link href={`/r/${sub.name}`}>
+                    <a className="font-bold hover:cursor-pointer">
+                      /r/${sub.name}
+                    </a>
+                  </Link>
+                  <p className="ml-auto font-medium">{sub.postCount}</p>
                 </div>
               ))}
             </div>
