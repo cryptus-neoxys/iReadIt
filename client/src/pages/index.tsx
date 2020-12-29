ef2101cddcbc5119d51c93254b4390b99717b4ef
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-30 03:37:07 +0530

    top subs ui begin

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,8 +2,9 @@ import Head from "next/head";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import useSWR from "swr";
+import Image from "next/image";
 
-import { Post } from "../types";
+import { Post, Sub } from "../types";
 import PostCard from "../components/PostCard";
 import { Fragment } from "react";
 
@@ -11,6 +12,7 @@ dayjs.extend(relativeTime);
 
 export default function Home() {
   const { data: posts } = useSWR("/posts");
+  const { data: topSubs } = useSWR("/misc/top-subs");
 
   return (
     <Fragment>
@@ -25,7 +27,31 @@ export default function Home() {
           ))}
         </div>
         {/* SideBar */}
-        <div>SideBar</div>
+        <div className="ml-6 w-80">
+          <div className="bg-white rounded">
+            <div className="p-4 border-b-2">
+              <p className="text-lg font-semibold text-center">
+                Top Communities
+              </p>
+            </div>
+            <div className="">
+              {topSubs?.map((sub: Sub) => (
+                <div
+                  key={sub.name}
+                  className="flex items-center px-4 py-2 text-xs border-b-4">
+                  <div className="rounded-full">
+                    <Image
+                      src={sub.imageUrl}
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
