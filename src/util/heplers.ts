aee5568fb05b1f56984ccc8e828fbe0fd697dcd6
= <=>
2021-01-21 15:24:46 +0530

    test change

--- src/util/heplers.ts
+++ src/util/heplers.ts
@@ -3,9 +3,9 @@ export function makeId(length: number): string {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
   for (let i = 0; i < length; i++) {
-    result += randomChars.charAt(
-      Math.floor(Math.random() * randomChars.length)
-    );
+    result =
+      result +
+      randomChars.charAt(Math.floor(Math.random() * randomChars.length));
   }
   return result;
 }
